93409f6f30bee0ae426e62e2fabdd579b74a54a5
Peter Jakubek <peterjakubek@gmail.com>
2022-03-09 21:19:20 +0000

    Fixing testing framework

---
+++ src/tests/features/bootstrap/FeatureContext.php
@@ -0,0 +1,41 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends DuskTestCase implements Context
+{
+    /** @var Browser $browser */
+    private $browser;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        parent::setUp();
+        // static::startChromeDriver();
+    }
+
+    /**
+     * @When /^homepage is visited$/
+     */
+    public function homepageIsVisited()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/');
+        });
+    }
+
+    /**
+     * @Then /^company greeting should be visible$/
+     */
+    public function companyGreetingShouldBeVisible()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->assertSee('Welcome, 15 Marketing');
+        });
+    }
+}
